a207365e1660160d68c4ce73dea7353309cfd5a6
SimoneAntonio11 <simoneantonio.dinaso@gmail.com>
2023-11-21 16:17:01 +0100

    asdsd

--- src/main/java/it/macros/app/repositories/RicettaRepository.java
+++ src/main/java/it/macros/app/repositories/RicettaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import it.macros.app.repositories.entities.Ricetta;
 
 public interface RicettaRepository extends JpaRepository <Ricetta, Integer> {
- 
+
 }
